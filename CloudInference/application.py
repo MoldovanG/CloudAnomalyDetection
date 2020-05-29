eea428c4247f8648ca6f9471f9c69076d1e07ff2
MoldovanG <george.alexandru.moldovan@gmail.com>
2020-05-29 20:51:19 +0300

    Changed the ObjectDetector to a fast one

--- CloudInference/application.py
+++ CloudInference/application.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, Response, jsonify
-import mxnet as mx
 from os import path
 
 import cv2
@@ -7,7 +6,7 @@ import time
 import numpy as np
 import pickle
 import json
-from gluoncv import model_zoo, data
+import cvlib as cv
 import tensorflow as tf
 from tensorflow.python.keras.backend import set_session
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
@@ -15,6 +14,7 @@ from keras.models import Model
 from keras.optimizers import Adam
 from multiprocessing.pool import ThreadPool
 import boto3
+
 
 class AutoEncoderModel:
     """
@@ -83,49 +83,31 @@ class AutoEncoderModel:
         return encodings[0]
 
 
+
 class ObjectDetector:
     """
     Class used for detecting objects inside a given image
 
      Parameters
     ----------
-    image = mxnet.NDarray - the image for which we want to extract the detections
+    image = np.array - the image for which we want to extract the detections
 
     Attributes
     ----------
-    net : pretrained-model from gluoncv, using ssd architecture trained on the coco dataset.
+    net : pretrained-model from cvlib, using yolov3-tiny architecture trained on the coco dataset.
     threshold : int - the threshold for the detections to be considered positive.
     """
-    net = model_zoo.get_model('ssd_512_mobilenet1.0_coco', pretrained=True, root='/tmp/')
     def __init__(self,image):
         self.image = image
-        self.threshold = 0.5
-        self.x_transformed_image, self.img_transformed_image = data.transforms.presets.ssd.transform_test(image, short=512)
-        self.class_IDs, self.scores, self.bounding_boxes = ObjectDetector.net(self.x_transformed_image)
-        self.bounding_boxes,self.scores,self.class_IDs = self.__clean_bounding_boxes_and_scores(self.bounding_boxes[0].asnumpy(), self.scores[0].asnumpy(),self.class_IDs[0].asnumpy())
-
-    def get_bounding_box_coordinates(self, bounding_boxes, index):
-        """
-        Parameters
-        ----------
-        bounding_boxes = np.array(Nx4)list containing all the bounding_boxes coordinates
-        index = int - the index of the wanted bounding-box
-
-        Returns
-        ----
-        A pair of 4 items(c1,l1,c2,l2)describing the top left and bottom right corners of the bounding box
-        """
-        c1 = int(bounding_boxes[index][0])
-        l1 = int(bounding_boxes[index][1])
-        c2 = int(bounding_boxes[index][2])
-        l2 = int(bounding_boxes[index][3])
-        return c1, l1, c2, l2
-
+        self.threshold = 0.65
+        print('Detecting on image with shape : ', image.shape)
+        print(type(image))
+        self.bounding_boxes, self.class_IDs, self.scores= cv.detect_common_objects(image,confidence = self.threshold, model = 'yolov3-tiny')
     def __get_cropped_detections(self,frame):
         cropped_images = []
         for idx,score in enumerate(self.scores):
             try:
-                c1, l1, c2, l2 = self.get_bounding_box_coordinates(self.bounding_boxes, idx)
+                c1, l1, c2, l2 = self.bounding_boxes[idx]
                 image = frame[l1:l2, c1:c2]
                 image = cv2.resize(image, dsize=(64, 64), interpolation=cv2.INTER_CUBIC)
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -150,7 +132,7 @@ class ObjectDetector:
         Wi = 64
         Hi = 64
         """
-        detections = self.__get_cropped_detections(self.img_transformed_image)
+        detections = self.__get_cropped_detections(self.image)
         return detections
 
 
@@ -158,44 +140,19 @@ class ObjectDetector:
         """
         Method that will return the detections for the image allready present in the ObjectDetector, and using the
         existent bounding-boxes, will also cropp the frames given as parameters.
-        :param frame_d3: mxnet.NDArray
-        :param frame_p3: mxnet.NDArray
+        :param frame_d3: np.array
+        :param frame_p3: np.array
         :return: A pair formed of :
                         - np.array containg detected object appearence of the t frame.
                         - np.array containg cropped image of the t-3 frame of the corresponding detected object
                         - np.array containg cropped image of the t+3 frame of the corresponding detected object
         """
-        z, img_d3 = data.transforms.presets.ssd.transform_test(frame_d3, short=512)
-        v, img_p3 = data.transforms.presets.ssd.transform_test(frame_p3, short=512)
-        detections = self.__get_cropped_detections(self.img_transformed_image)
-        cropped_d3 = self.__get_cropped_detections(img_d3)
-        cropped_p3 = self.__get_cropped_detections(img_p3)
+        detections = self.__get_cropped_detections(self.image)
+        cropped_d3 = self.__get_cropped_detections(frame_d3)
+        cropped_p3 = self.__get_cropped_detections(frame_p3)
 
         return detections,cropped_d3,cropped_p3
 
-    def __clean_bounding_boxes_and_scores(self, bounding_boxes, scores, class_ids):
-        """
-        Method used for removing the bounding boxes that have a score under the set threshold
-        :param bounding_boxes: bounding_boxes np.array with size(Nx4)
-        :param scores: scores np.array with size(Nx1)
-        :param class_ids: class_ids np.array with size(Nx1)
-        :return: Trimmed bounding_boxes as np.array, and trimmed scoresas np.array
-        """
-        bboxes = []
-        new_scores = []
-        new_class_ids=[]
-        counter = 0
-        while scores[counter] > self.threshold:
-            c1, l1, c2, l2 = self.get_bounding_box_coordinates(bounding_boxes, counter)
-            if c1 < 0 or c2 > self.img_transformed_image.shape[1] or l1 < 0 or l2 > self.img_transformed_image.shape[0]:
-                print("Invalid bounding box:", c1,l1,c2,l2)
-                counter = counter + 1
-                continue
-            new_class_ids.append(int(class_ids[counter]))
-            bboxes.append(bounding_boxes[counter])
-            new_scores.append(scores[counter])
-            counter = counter + 1
-        return np.array(bboxes),np.array(new_scores),np.array(new_class_ids)
 
 class GradientCalculator:
 
@@ -248,12 +205,12 @@ def load_frame(arg):
     frame_name = arg[0] 
     s3_client = arg[1]
     frame_path = path.join("/tmp/",frame_name)
-    # print("Loading frame with name : ", frame_name)
     result = s3_client.download_file("moldovan.inferenceframes", frame_name, frame_path)
     s3 = boto3.resource('s3')
     s3.Object("moldovan.inferenceframes", frame_name).delete()
     # Read the frame
-    frame = mx.nd.load(frame_path)[0]
+    frame = np.load(frame_path)
+    print("Loaded frame with shape : ", frame.shape)
     return frame
 
 
@@ -296,28 +253,21 @@ def lambda_handler(file_key):
     
     # Write request body data into file
     frame_name = file_key
+    print('Got the key :', frame_name)
     arguments_tuples = [(frame_name,s3_client),(frame_name+"_d3", s3_client),(frame_name+"_p3", s3_client)]
     pool = ThreadPool(processes=4)
     results = pool.map(load_frame, arguments_tuples)
     frame = results[0]
     frame_d3 = results[1]
     frame_p3 = results[2]
-
     feature_vectors, bounding_boxes = get_feature_vectors_and_bounding_boxes(frame_predictor,frame,frame_d3,frame_p3)
     frame_score = 0
     boxes = []
-    x, img = data.transforms.presets.ssd.transform_test(frame, short=512)
-    ratio1 = img.shape[0]/frame.shape[0]
-    ratio2 = img.shape[1]/frame.shape[1]
 
     for idx, vector in enumerate(feature_vectors):
         score = frame_predictor.get_inference_score(vector)
         if score > frame_predictor.threshold:
             c1,l1,c2,l2 = bounding_boxes[idx]
-            c1 = int(c1/ratio2)-1
-            c2 = int(c2/ratio2)-1
-            l1 = int(l1/ratio1)-1
-            l2 = int(l2/ratio2)-1
             boxes.append([c1,l1,c2,l2])
         if  score > frame_score:
             frame_score = score
@@ -335,4 +285,4 @@ def hello_world():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    app.run(host='0.0.0.0', debug = False)
+    app.run(host='0.0.0.0', debug = True)
